cdc0917c7164e804652257f2eac121b50f604fbb
alinebeda <9097@holbertonstudents.com>
2024-05-17 15:54:11 +0000

    complete

---
+++ python-data_structures/9-max_integer.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+def max_integer(my_list=[]):
+    if not my_list:
+        return (None)
+    max_value = my_list[0]
+
+    i = 1
+    while i < len(my_list):
+        if my_list[i] > max_value:
+            max_value = my_list[i]
+        i += 1
+
+    return (max_value)
