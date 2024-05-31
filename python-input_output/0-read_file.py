559d67675b3bec0744bedf4e9cbaf91188b542b2
alinebeda <9097@holbertonstudents.com>
2024-05-31 19:44:50 +0000

    again

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,7 +1,13 @@
 #!/usr/bin/python3
 """Module defines the read_file function"""
 
+
 def read_file(filename=""):
-    """Read and print text from file"""
-    with open(filename, mode="r", encoding="utf-8") as f:
-        print(f.read(), end="")
+    """Reads a file and prints to stdout
+
+    Args:
+        filename (str, optional): name of file to read
+    """
+    with open(filename, 'r', encoding='utf-8') as file:
+        for line in file:
+            print(line, end='')
